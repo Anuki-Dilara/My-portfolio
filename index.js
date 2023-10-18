d8d70e6d924ada5ccbcae3fb49ff1e2e4c55f227
Anuki Dilara <123262067+Anu20022@users.noreply.github.com>
2023-10-18 20:57:21 +0530

    about

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 
-/*var typed = new Typed(".auto-typed",{
-    strings: ["Front-End Developer","Graphic Designer"],
-    typeSpeed: 150,
-    backSped: 150,
-    loop: true
-})
-var type = new Type(".auto-type",{
-    strings: ["Anuki Dilara","Anuki Dilara"],
-    typeSpeed: 150,
-    backSped: 150,
-    loop: true
-})*/
-
-/*let text = document.querySelector(".text p");
-
-text.innerHTML = text.innerHTML.split("").map((char,i)=>
-    '<b style="transform:rotate(${i * 6.3}deg")>${char}</b>'
-).join("");*/
-
 const text = document.querySelector(".sec-text");
 const textLoad = () => {
     setTimeout(() =>{
