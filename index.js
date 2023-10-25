9dc8874ebb5eddc58875e73c5e0641ac0151ddbb
Anuki Dilara <123262067+Anu20022@users.noreply.github.com>
2023-10-25 20:25:58 +0530

    home

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+/*scroll section*/
+let sections = document.querySelectorAll('section');
+let section = document.querySelectorAll('header nav a');
 
+window.onscroll = () =>{
+    /*sticky header*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>100);
+
+    /*remove icon and navbar when click navbar*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+/*menu icon*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar =document.querySelector('.nav_bar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/*home section multi typing*/
 const text = document.querySelector(".sec-text");
 const textLoad = () => {
     setTimeout(() =>{
@@ -11,8 +35,28 @@ const textLoad = () => {
 textLoad();
 setInterval(textLoad,8000);
 
-/*
-let text = document.querySelector(".text p");
-text.innerHTML = text.innerHTML.split("").map((char, i) =>
-    `<b style="transform:rotate(${i * 10}deg)">${char}</b>`
-).join("");*/
+/*portfolio filter*/
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+
+for (let  i =0; i< filterButtons.length; i++){
+    filterButtons[i].addEventListener("click", function (){
+        for (let  j=0; j<filterButtons.length;j++){
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+
+        for (let k=0; k<items.length;k++){
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")){
+                items[k].style.display = "block";
+            }
+            if (target == "all"){
+                items[k].style.display = "block";
+            }
+        }
+    })
+
+}
+
